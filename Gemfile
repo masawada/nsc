7c431d46d763421ff2ea2d66299702b715913821
Masayoshi Wada <developer@andantesoftware.com>
2016-01-10 16:10:20 +0900

    雑にできた

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'dotenv'
 gem 'aws-sdk'
+gem 'simple_color'
--- client.rb
+++ client.rb
@@ -1,5 +1,6 @@
 require 'dotenv'
 require 'aws-sdk'
+require 'simple_color'
 
 Dotenv.load
 AWS_ACCESS_KEY = ENV["AWS_ACCESS_KEY"]
@@ -51,6 +52,68 @@ class Client
       region: region,
       force_path_style: true
     )
+
+    @color = SimpleColor.new
+  end
+
+  def logger(color, message)
+    if color
+      @color.echos(color, message)
+    else
+      puts message
+    end
+  end
+
+  def info(message)
+    logger(nil, message)
+  end
+
+  def error(message)
+    logger(:red, message)
+  end
+
+  def fetch(opts)
+    source = opts.shift
+    dst = opts.shift
+    if source.empty?
+      error('source not defined')
+      return
+    end
+
+    if dst.empty?
+      error('dst not defined')
+      return
+    end
+
+    bucket_name, object_key = source.split('/', 2)
+    File.open(dst, "w") do |file|
+      @client.get_object(bucket: bucket_name, key: object_key) do |chunk|
+        file.write(chunk)
+      end
+    end
+  end
+
+  def post(opts)
+    source = opts.shift
+    dst = opts.shift
+    if source.empty?
+      error('source not defined')
+      return
+    end
+
+    if dst.empty?
+      error('dst not defined')
+      return
+    end
+
+    file = File.open(source)
+    file_name = File.basename(source)
+
+    @client.put_object(
+        bucket: dst,
+        body: file,
+        key: file_name
+    )
   end
 
   def list(bucket_list)
@@ -71,15 +134,43 @@ class Client
       end
     end
   end
+
+  def create_bucket(bucket_list)
+    if bucket_list.nil? || bucket_list.empty?
+      error('no bucket names defined')
+      return
+    end
+
+    bucket_list.each do |bucket_name|
+      @client.create_bucket(bucket: bucket_name)
+    end
+  end
+
+  def remove_bucket(bucket_list)
+    if bucket_list.nil? || bucket_list.empty?
+      error('no bucket names defined')
+      return
+    end
+
+    bucket_list.each do |bucket_name|
+      @client.delete_bucket(bucket: bucket_name)
+    end
+  end
 end
 
 # main
 client = Client.new(AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_ENDPOINT)
 case params
 when Command.fetch?
+  client.fetch(ARGV)
 when Command.post?
+  client.post(ARGV)
 when Command.list?
   client.list(ARGV)
+when Command.create_bucket?
+  client.create_bucket(ARGV)
+when Command.remove_bucket?
+  client.remove_bucket(ARGV)
 else
   puts "no commands detected"
 end
